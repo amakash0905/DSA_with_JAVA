8533482ed43b23d3cb9aafec3647b2185246a367
amakash0905 <akash.lawrencian2020@gmail.com>
2025-08-08 16:24:32 +0530

    fifth prog commited

--- prog5.java
+++ prog5.java
@@ -30,6 +30,7 @@ class prog5 {
     }
 } */
 
+/* 
 class prog5 {
     public static void rotate(int[] nums, int k) {
         int n=nums.length;
@@ -45,6 +46,35 @@ class prog5 {
             System.out.print(ele+" ");
         }
     }
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5,6,7};
+        int k=3;
+        rotate(nums,k);
+    }
+} */
+
+class prog5 {
+    public static void rotate(int[] nums, int k) {
+        int n=nums.length;
+        int d=k%n;
+        reverse(nums, n-d, n-1);
+        reverse(nums, 0, n-d-1);
+        reverse(nums, 0, n-1);
+        for (int i : nums) {
+            System.out.print(i+" ");
+        }    
+    }
+
+    public static void reverse(int[] nums, int a, int b) {
+        while(a<b) {
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums={1,2,3,4,5,6,7};
         int k=3;
